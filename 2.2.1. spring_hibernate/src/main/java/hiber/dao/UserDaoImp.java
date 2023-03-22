b3fd57352148da89844bff8741f5c99c754b654d
EdivryCezar <117872312+EdivryCezar@users.noreply.github.com>
2023-03-23 02:49:06 +0300

    Задание выполнено

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,7 +1,9 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -26,4 +28,12 @@ public class UserDaoImp implements UserDao {
       return query.getResultList();
    }
 
+   @Override
+   public User getCarsByUser(String model, int series) {
+      Query query = sessionFactory.getCurrentSession()
+              .createQuery("from User as u where u.car.model = :paramModel and u.car.series = :paramSeries")
+              .setParameter("paramModel", model)
+              .setParameter("paramSeries", series);
+      return (User) query.getSingleResult();
+   }
 }
